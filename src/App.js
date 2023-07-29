7c4edbc55726ca8d2a8093c84c34e22e5605dee7
unknown <you@example.com>
2023-07-29 16:40:59 +0545

    API add

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import Header from "./components/header.js";
 import Frame from "./components/frame.js";
 import Products from "./components/Products";
 import FeatureCard from "./components/FeaturesCard";
+import StatCard from "./components/StatCard";
+import Footer from "./components/Footer";
+import Home from "./components/modules";
+import { Routes, Route } from "react-router-dom";
+import Product from "./components/modules";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<Product />} />
+      </Routes>
       <Frame />
-      <h2 className=" text-4xl font-bold text-center mt-20">Products</h2>
+
       <Products />
       <FeatureCard />
+      <StatCard />
+      <Footer />
     </div>
   );
 };
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { BiCartDownload } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+const navigations = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Products",
+    path: "/products",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+];
 
 const Header = () => {
   return (
-    <header class="text-gray-600 body-font shadow-lg">
-      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+    <header className="text-gray-600 body-font shadow-lg">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <Link
+          to={"/"}
+          className="flex cursor-pointer title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -12,21 +36,24 @@ const Header = () => {
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            class="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
+            className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
             viewBox="0 0 24 24"
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
           <span class="ml-3 text-xl">Ecommerce</span>
-        </a>
-        <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a class="mr-5 hover:text-gray-900">First Link</a>
-          <a class="mr-5 hover:text-gray-900">Second Link</a>
-          <a class="mr-5 hover:text-gray-900">Third Link</a>
-          <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+        </Link>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          {navigations.map((navigations) => {
+            return (
+              <Link to={navigations.path} className="mr-5 hover:text-gray-900">
+                {navigations.name}
+              </Link>
+            );
+          })}
         </nav>
-        <button class="inline-flex items-center text-white bg-green-500 border-0 py-2 px-4 focus:outline-none hover:bg-green-400 rounded text-base mt-4 md:mt-0">
-          🛒
+        <button class="inline-flex items-center text-white bg-green-500 border-0 py-2 px-4 focus:outline-none hover:bg-green-400 rounded text-base mt-4 md:mt-0 h-15 w-20">
+          <BiCartDownload className="h-10 w-20" />
           <svg
             fill="none"
             stroke="currentColor"
